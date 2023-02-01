f168186ab5fb2442196f98d34f86d1d9edb632f3
Abraham Keleta <keletaabraham@gmail.com>
2023-02-01 12:11:42 -0800

    adding memes

--- app.py
+++ app.py
@@ -1,15 +1,14 @@
 # Import libraries that we will use
 from flask import Flask, render_template, request, redirect
 from flask_sqlalchemy import SQLAlchemy
-import json
 from datetime import datetime
-
 
 # This is how we initiate a flask app
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
 app.config['SECRET_KEY'] = 'thisisasecretkey'
-db = SQLAlchemy(app)
+db = SQLAlchemy()
+db.init_app(app)
 
 # Create the meme model 
 class Meme(db.Model):
@@ -18,27 +17,19 @@ class Meme(db.Model):
     date_created = db.Column(db.DateTime, default=datetime.utcnow)
 
 
-# This function will load our json file 
-# and return both meme-title and meme-url
-def get_memes():
-    with open('memes.json') as data_file:    
-        data = json.load(data_file)
-        return data['memes']
-
-
 # This function will load our website naked. Meaning nothing behind the /. 
-@app.route("/", methods=["POST", "GET"])
+@app.route('/', methods=['POST', 'GET'])
 def index():
     if request.method == 'POST':
-        meme_url = request.form['content']
-        new_meme = Meme(img_src=meme_url)
+        meme_content = request.form['content']
+        new_meme = Meme(content=meme_content)
 
         try:
             db.session.add(new_meme)
             db.session.commit()
             return redirect('/')
         except:
-            return 'There was an issue adding your task'
+            return 'There was an issue adding your Meme'
 
     else:
         memes = Meme.query.order_by(Meme.date_created).all()
@@ -46,8 +37,10 @@ def index():
 
 
 # This line will run our server on this port 80
-if __name__ == '__main__':
-    app.run(debug=True)
+if (__name__ == "__main__"):
+    app.run(host="0.0.0.0", port=80)
+
 
 
 # YOUR GOAL IS TO ADD MORE MEMES AND SHOW THEM ALL IN THE PAGE USING A LOOP
+
